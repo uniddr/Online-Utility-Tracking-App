af29a93d80b2290e9b27ee2b81c6de831dbb71df
uniddr <52971354+uniddr@users.noreply.github.com>
2021-10-17 09:18:15 +0600

    Completed admin dashboard

--- js/routes.js
+++ js/routes.js
@@ -18,7 +18,7 @@ route.use(session(
         secret : 'utehss3eu7uh',
         resave : true,
         saveUninitialized : true,
-        cookie : { maxAge : 1000 * 60 * 3 }
+        cookie : { maxAge : 1000 * 60 * 6 }
     }
 ));
 route.use(express.urlencoded({ extended : true }));
@@ -54,7 +54,7 @@ route.post('/auth',function(request, response)
         {
             if ((typeof request.body.submit !== 'undefined') && password && (typeof request.body.forpass === 'undefined'))
             {
-                var qry = 'select*from sakila.node_cred where Username = ? and Password = ?';
+                var qry = 'select*from sakila.node_users where Username = ? and Password = ?';
                 connection.query(qry, [username, password], function(error, results, fields)
                 {
                     if(error) throw error;
@@ -74,7 +74,7 @@ route.post('/auth',function(request, response)
                 
             }else if ((typeof request.body.forpass !== 'undefined') && (typeof request.body.submit === 'undefined'))
             {
-                var qry = 'select*from sakila.node_cred where Username = ?';
+                var qry = 'select*from sakila.node_users where Username = ?';
                 connection.query(qry, [username], function(error, results, fields)
                 {
 
@@ -117,20 +117,173 @@ route.get('/dashboard', function(request, response)
     {
         if(request.session.loggedin)
         {
-            response.send('Welcome, ID ' + request.session.userid
-             + `\n<a href='/logout'>Click here to logout</a>`);
+            response.sendFile(path.join(__dirname, '..', 'public', 'html', 'dashboard.html'));
+            // response.send('Welcome, ID ' + request.session.userid
+            //  + `\n<a href='/logout'>Click here to logout</a>`);
         }else
         {
             response.send('Please login to view this page!');
+            response.end();
         }
-        response.end();
     }
 );
 
-route.get('/logout', function(request, response)
+route.post('/logout', function(request, response)
     {
-        request.session.destroy();
-        response.redirect('/');
+        console.log(request.session)
+        request.session.destroy(() => {
+            response.redirect('/');
+        });
+    }
+);
+
+// route.get('/logout', function(request, response)
+//     {
+//         request.session.destroy(() => {
+//             response.redirect('/');
+//         });
+//     }
+// );
+
+route.post('/infocont', function(request, response)
+    {
+        let rsrctype = request.body.resource;
+        let infoperiod = request.body.period;
+        var cc = -1;
+        var tp = 1.0800;
+        var td = 2.0400;
+        var tc = 3.0050;
+
+        // console.log(rsrctype);
+        // console.log(infoperiod);
+        // console.log(request.body);
+        if(rsrctype == "Water")
+        {
+            var qry1 = "select count( distinct(id)) as tcc from sakila.node_users where User_type='C' and Water_service='Yes'";
+
+            if(infoperiod == "This Month"){
+                var qry2 = "select sum(paid_amount) as ttp from sakila.node_water_bill where month(payment_date)=month(now()) and year(payment_date)=year(now())";
+                var qry3 = "select sum(water_due) as ttd from sakila.node_users where Water_service='Yes'";
+                var qry4 = "select sum(used_amount) as ttc from sakila.node_usage_history where month(record_date)=month(now()) and year(record_date)=year(now()) and resource_type='Water'";
+            }
+            else if(infoperiod == "This Year")
+            {
+                var qry2 = "select sum(paid_amount) as ttp from sakila.node_water_bill where year(payment_date)=year(now())";
+                var qry3 = "select sum(water_due) as ttd from sakila.node_users where Water_service='Yes'";
+                var qry4 = "select sum(used_amount) as ttc from sakila.node_usage_history where year(record_date)=year(now()) and resource_type='Water'";
+            }
+            else if(infoperiod == "All Time")
+            {
+                var qry2 = "select sum(paid_amount) as ttp from sakila.node_water_bill";
+                var qry3 = "select sum(water_due) as ttd from sakila.node_users where Water_service='Yes'";
+                var qry4 = "select sum(used_amount) as ttc from sakila.node_usage_history where resource_type='Water'";
+            }
+        }
+        else if(rsrctype == "Electricity")
+        {
+            var qry1 = "select count( distinct(id)) as tcc from sakila.node_users where User_type='C' and Elec_service='Yes'";
+
+            if(infoperiod == "This Month"){
+                var qry2 = "select sum(paid_amount) as ttp from sakila.node_electricity_bill where month(payment_date)=month(now()) and year(payment_date)=year(now())";
+                var qry3 = "select sum(Elec_due) as ttd from sakila.node_users where Elec_service='Yes'";
+                var qry4 = "select sum(used_amount) as ttc from sakila.node_usage_history where month(record_date)=month(now()) and year(record_date)=year(now()) and resource_type='Electricity'";
+            }
+            else if(infoperiod == "This Year")
+            {
+                var qry2 = "select sum(paid_amount) as ttp from sakila.node_electricity_bill where year(payment_date)=year(now())";
+                var qry3 = "select sum(Elec_due) as ttd from sakila.node_users where Elec_service='Yes'";
+                var qry4 = "select sum(used_amount) as ttc from sakila.node_usage_history where year(record_date)=year(now()) and resource_type='Electricity'";
+            }
+            else if(infoperiod == "All Time")
+            {
+                var qry2 = "select sum(paid_amount) as ttp from sakila.node_electricity_bill";
+                var qry3 = "select sum(Elec_due) as ttd from sakila.node_users where Elec_service='Yes'";
+                var qry4 = "select sum(used_amount) as ttc from sakila.node_usage_history where resource_type='Electricity'";
+            }
+        }
+        connection.query(qry1, function(error1, result1, fields1)
+        {
+            if(error1) throw error1;
+            if(result1.length > 0)
+            {
+                if(result1[0].tcc != 'null')
+                {
+                    cc = result1[0].tcc;
+                }
+                else
+                {
+                    cc = result1[0].tcc;
+                }
+                // console.log(result1);
+                // call the other 3 connection.query() functions from here,
+                // one inside the callback of the other. The response.setHeader,send,end
+                // will go inside the innermost callback.
+                connection.query(qry2, function(error2, result2, fields2) {
+                    if(error2) throw error2;
+                    if(result2.length > 0)
+                    {
+                        if(result2[0].ttp != 'null')
+                        {
+                            tp = result2[0].ttp;
+                        }
+                        else
+                        {
+                            tp = result2[0].ttp;
+                        }
+
+                        connection.query(qry3, function(error3, result3, fields3) {
+                            if(error3) throw error3;
+                            if(result3.length > 0)
+                            {
+                                if(result3[0].ttd != 'null')
+                                {
+                                    td = result3[0].ttd;
+                                }
+                                else
+                                {
+                                    td = 0.0;
+                                }
+
+                                connection.query(qry4, function(error4, result4, fields4) {
+                                    if(error4) throw error4;
+                                    if(result4.length > 0)
+                                    {
+                                        if(result4[0].ttc != 'null')
+                                        {
+                                            tc = result4[0].ttc;
+                                        }
+                                        else
+                                        {
+                                            tc = 0.0;
+                                        }
+
+                                        response.setHeader('Content-Type', 'application/json');
+                                        response.send({curclients: cc, totalp: tp, totald: td, totalc: tc});
+                                        response.end();
+                                    }
+                                    else
+                                    {
+                                        response.status(404).end('Error fetching tc!');
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                response.status(404).end('Error fetching td!');
+                            }
+                        });
+                    }
+                    else
+                    {
+                        response.status(404).end('Error fetching tp!');
+                    }
+                });
+            }
+            else
+            {
+                response.status(404).end('Error fetching cc!');
+            }
+        });
     }
 );
 
---
+++ public/js/dashboard.js
@@ -0,0 +1,269 @@
+var lmc = document.getElementById("leftmenucont");
+var pboc = document.getElementById("profbtnoptcont");
+var rtoc = document.getElementById("rsrctypeoptcont");
+var ipoc = document.getElementById("infoperiodoptcont");
+
+document.addEventListener('click', function() {
+    lmc.style.width = "0";
+
+    pboc.style.display = "none";
+
+    rtoc.style.height = "0";
+
+    ipoc.style.height = "0";
+});
+
+lmc.addEventListener("transitionend", function() {
+    if(document.getElementById("leftmenuopt").style.display == "block")
+    {
+        document.getElementById("leftmenuopt").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("leftmenuopt").style.display = "block";
+    }
+});
+
+lmc.addEventListener("transitionstart", function() {
+    document.getElementById("leftmenuopt").style.display = "none";
+});
+
+document.getElementById("menuicon").addEventListener('click', function() {
+    pboc.style.display = "none";
+
+    rtoc.style.height = "0";
+
+    ipoc.style.height = "0";
+
+    lmc.style.width = "200px";
+    event.stopPropagation();
+});
+
+document.getElementById("leftmenuback").addEventListener("click", function() {
+    lmc.style.width = "0";
+    event.stopPropagation();
+});
+
+document.getElementById("profcont").addEventListener('click', function() {
+    lmc.style.width = "0";
+
+    pboc.style.display = "inline";
+
+    rtoc.style.height = "0";
+
+    ipoc.style.height = "0";
+    event.stopPropagation();
+});
+
+rtoc.addEventListener("transitionend", function() {
+    if(document.getElementById("rsrctypeoptvalcont").style.display == "block")
+    {
+        this.style.removeProperty("box-shadow");
+        document.getElementById("rsrctypeoptvalcont").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("rsrctypeoptvalcont").style.display = "block";
+    }
+});
+
+rtoc.addEventListener("transitionstart", function() {
+    if(document.getElementById("rsrctypeoptvalcont").style.display == "none")
+    {
+        this.style.boxShadow = "1px 1px 10px 3px rgba(0, 0, 0, 0.336)";
+    }
+});
+
+document.getElementById("rsrctypeupbtn").addEventListener('click', function() {
+    lmc.style.width = "0";
+    pboc.style.display = "none";
+
+    document.getElementById("rsrctypeoptvalcont").style.display = "none";
+    rtoc.style.height = "52px";
+
+    ipoc.style.height = "0";
+
+    let cur = document.getElementById("rsrctypedefaultinfo").innerHTML;
+    let arr = document.getElementById("rsrctypeoptvalcont").getElementsByTagName("a");
+    for(let i=0; i < arr.length; i++)
+    {
+        let arrspan = arr[i].getElementsByTagName("span");
+        if(arrspan[0].innerHTML == cur)
+        {
+            arr[i].getElementsByTagName("img")[0].style.display = "inline";
+        }
+        else
+        {
+            arr[i].getElementsByTagName("img")[0].style.display = "none";
+        }
+    }
+    event.stopPropagation();
+});
+
+ipoc.addEventListener("transitionend", function() {
+    if(document.getElementById("infoperiodoptvalcont").style.display == "block")
+    {
+        this.style.removeProperty("box-shadow");
+        document.getElementById("infoperiodoptvalcont").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("infoperiodoptvalcont").style.display = "block";
+    }
+});
+
+ipoc.addEventListener("transitionstart", function() {
+    if(document.getElementById("infoperiodoptvalcont").style.display == "none")
+    {
+        this.style.boxShadow = "1px 1px 10px 3px rgba(0, 0, 0, 0.336)";
+    }
+});
+
+document.getElementById("infoperiodupbtn").addEventListener('click', function() {
+    lmc.style.width = "0";
+    pboc.style.display = "none";
+    
+    rtoc.style.height = "0";
+
+    document.getElementById("infoperiodoptvalcont").style.display = "none";
+    ipoc.style.height = "78px";
+
+    let cur = document.getElementById("infoperioddefaultinfo").innerHTML;
+    let arr = document.getElementById("infoperiodoptvalcont").getElementsByTagName("a");
+    for(let i=0; i < arr.length; i++)
+    {
+        let arrspan = arr[i].getElementsByTagName("span");
+        if(arrspan[0].innerHTML == cur)
+        {
+            arr[i].getElementsByTagName("img")[0].style.display = "inline";
+        }
+        else
+        {
+            arr[i].getElementsByTagName("img")[0].style.display = "none";
+        }
+    }
+    event.stopPropagation();
+});
+
+document.getElementById("leftmenucont").addEventListener('click', function() {
+    event.stopPropagation();
+});
+
+function setinfocontval() {
+    let rsrctype = document.getElementById("rsrctypedefaultinfo").innerHTML;
+    let infoperiod = document.getElementById("infoperioddefaultinfo").innerHTML;
+
+    let dataToSend = {
+        resource: rsrctype,
+        period: infoperiod
+    };
+
+    jQuery.ajax({
+        url: "/infocont",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(dataToSend),
+        success: function(result, status, xhr) {
+            if(status == "success")
+            {
+                
+                document.getElementById("ccvalue").innerHTML = `${result.curclients}`;
+                document.getElementById("tpvalue").innerHTML = `${result.totalp}`;
+                document.getElementById("tdvalue").innerHTML = `${result.totald}`;
+                document.getElementById("tcvalue").innerHTML = `${result.totalc}`;
+            }
+            else
+            {
+                alert("Failed to retrieve!");
+            }
+        },
+        complete: function(xhr, status) {
+            if(status == "success")
+            {
+                // alert("Success");
+            }
+        }
+    });
+}
+jQuery(document).ready(function() {
+    document.getElementsByClassName("optvalcont")[0].getElementsByTagName("a")[0].click();
+});
+
+function optclickhandler(caller) {
+    let sp = caller.getElementsByTagName("span")[0];
+
+    if(sp.id == "rsrctypeoptval0")
+    {
+        document.getElementById("tctitle").innerHTML = "Total Consumption (gal)";
+        document.getElementById("rsrctypedefaultinfo").innerHTML = sp.innerHTML;
+        rtoc.style.height = "0";
+        setinfocontval();
+    }
+    else if(sp.id == "rsrctypeoptval1")
+    {
+        document.getElementById("tctitle").innerHTML = "Total Consumption (kwh)";
+        document.getElementById("rsrctypedefaultinfo").innerHTML = sp.innerHTML;
+        rtoc.style.height = "0";
+        setinfocontval();
+    }
+    else if(sp.id == "infoperiodoptval0")
+    {
+        document.getElementById("infoperioddefaultinfo").innerHTML = sp.innerHTML;
+        ipoc.style.height = "0";
+        setinfocontval();
+    }
+    else if(sp.id == "infoperiodoptval1")
+    {
+        document.getElementById("infoperioddefaultinfo").innerHTML = sp.innerHTML;
+        ipoc.style.height = "0";
+        setinfocontval();
+    }
+    else if(sp.id == "infoperiodoptval2")
+    {
+        document.getElementById("infoperioddefaultinfo").innerHTML = sp.innerHTML;
+        ipoc.style.height = "0";
+        setinfocontval();
+    }
+    event.stopPropagation();
+}
+
+var val = document.getElementsByClassName("optvalcont");
+val[0].getElementsByTagName("a")[0].addEventListener("click", function() {
+    optclickhandler(this);
+});
+val[0].getElementsByTagName("a")[1].addEventListener("click", function() {
+    optclickhandler(this);
+});
+val[1].getElementsByTagName("a")[0].addEventListener("click", function() {
+    optclickhandler(this);
+});
+val[1].getElementsByTagName("a")[1].addEventListener("click", function() {
+    optclickhandler(this);
+});
+val[1].getElementsByTagName("a")[2].addEventListener("click", function() {
+    optclickhandler(this);
+});
+
+document.getElementById("profbtnoptcont").getElementsByTagName("a")[0].addEventListener("click", function() {
+    jQuery.ajax({
+        async: "true",
+        url: "/logout",
+        type: "POST",
+        success: function(result, status, xhr) {
+            if(status == "success")
+            {
+                // alert(`${xhr.status}: ${xhr.statusText}`);
+            }
+            else
+            {
+                alert("Failed to logout!");
+            }
+        },
+        complete: function(xhr, status) {
+            if(status == "success")
+            {
+                window.location.href = "/";
+            }
+        }
+    });
+});
